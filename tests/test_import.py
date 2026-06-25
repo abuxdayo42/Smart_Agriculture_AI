bba2e84fc0662cc02e9c286eac26b23fb9913785
Allah Bux <abuxdayo42@gmail.com>
2026-06-25 12:37:42 +0500

    Added automated testing and Docker build to CI pipeline

---
+++ tests/test_import.py
@@ -0,0 +1,14 @@
+def test_import_app():
+    import app
+
+
+def test_import_yield():
+    import pages.Crop_Yield_Prediction
+
+
+def test_import_fertilizer():
+    import pages.Fertilizer_Recommendation
+
+
+def test_import_disease():
+    import pages.Disease_Detection
